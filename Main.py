4ca85d6a2fef10f0e31933710a7e10cef79763e1
cantleym01 <macbad2012@gmail.com>
2017-10-11 17:12:46 -0400

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,4 +1,4 @@
-from newspaper import Article
+#from newspaper import Article
 from rake_nltk import Rake
 
 # parameters are as follows:
@@ -6,13 +6,32 @@ from rake_nltk import Rake
 # numOfKeywords (integer: how many keywords to obtain from text)
 def getKeywords(text, numOfKeywords):
 
+    # remove words which are not valid keywords or are not useful
+    # parameters are as follows:
+    # words (list of strings: words to scrub of junk data and irrelevant data)
+    def scrubList(words):
+
+        invalidWords = [u'\u201d', u'\u201c', u'\u2019', u'\u2018', u'\u2014']
+
+        i = 0
+        while (i < len(words)):
+            # if the word is invalid, remove it (don't increment iteration to stay in range)
+            if (words[i] in invalidWords):
+                del words[i]
+            else:  # otherwise increment iteration
+                i += 1
+
+        return words
+
     # clean keywords; some keywords from rake have junk in them
     # parameters are as follows:
     # words (list of strings: words to scrub of junk data and irrelevant data)
     def scrubWord(word):
         word = word.replace(",", "")
         word = word.replace(".", "")
+
         return word
+
     rak = Rake()  # english by default
 
     # extract keywords and store them+their degree in a dictionary
@@ -20,9 +39,6 @@ def getKeywords(text, numOfKeywords):
     wordDic = rak.get_word_degrees()
 
     rankedWords = sorted(wordDic, key=wordDic.get, reverse=True)
-    # remove words which are not valid keywords or are not useful
-    invalidWords = [u'\u201d', u'\u201c', u'\u2019', u'\u2018', u'\u2014']
-    rankedWords = [word for word in rankedWords if word not in invalidWords]
     rankedWords = scrubList(rankedWords)
     returnDic = {}
 
@@ -38,15 +54,12 @@ def getKeywords(text, numOfKeywords):
 
     return returnDic
 
-if __name__ == '__main__':
-   url = 'https://techcrunch.com/2017/10/06/apple-is-looking-into-reports-of-iphone-8-batteries-swelling/'
-   
-   # newspaper
-   art = Article(url, language='en')  # English
-   art.download()
-   art.parse()
-   print(getKeywords(art.text, 10))
-   art.nlp();
-   
-   #note art.keywords, art.title
-   print(art.keywords);
+#test case (Don't forget to uncomment newspaper import)
+#url = 'https://techcrunch.com/2017/10/06/apple-is-looking-into-reports-of-iphone-8-batteries-swelling/'
+
+# newspaper
+#art = Article(url, language='en')  # English
+#art.download()
+#art.parse()
+
+#print(getKeywords(art.text, 10))
