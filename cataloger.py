7b9b43763b88cebe44f15603cbf74ba7603307a6
Alex Entrekin <adentrekin@mix.wvu.edu>
2017-10-28 12:31:18 -0400

    Confirmed: Working for keyword weights. Will work on search later today...

--- cataloger.py
+++ cataloger.py
@@ -7,40 +7,27 @@ driver = GraphDatabase.driver("bolt://35.197.88.141:7687", auth=basic_auth("neo4
 
 
 def batchKeys():
-   result = driver.session().run("match (a:Article {keywordProcessed : false}) RETURN a.link AS url")
+   result = driver.session().run("match (a:Article) WHERE NOT exists(a.keywordTime) RETURN a.link AS url LIMIT 3")
    for record in result:
-      processKeys(record['url'])
+      dbAdd(record['url'])
 
-def processKeys(url):
-   try:
-      art = Article(url)
-      art.download()
-      art.parse()
-      art.nlp() #need to use custom keyword extractor here
-      request = "MATCH (a:Article { link : '{url}' } ) SET a.keywordProcessed = true FOREACH ( key IN {keywords} | MERGE (a)-[:Has]->(:Keyword {name : key}) )"
-      driver.session().run(request, keywords=art.keywords, url=url)
-      print('ding!')
-   except Exception as err:
-      print("Error")
-      print(err)
-      print(url)   
-      print("")     
 
-def dbAdd(url, keyDict=None, labels=dict()):
+def dbAdd(url, keyDict=None):
    if keyDict==None:
       art = Article(url)
       art = Article(url, language='en')  # English
       art.download()
       art.parse()
-      keyDict = Main3.getKeywords(art.text, 10)
-      labels['title'] = art.title
-   #untested, but hopeful
+      keyDict = Main3.getKeywords(art.text, 20)
+      #print(keyDict)
+      keyDict = dict( [ (key.decode('ascii'), value) for key, value in keyDict.items() ])
+      #print(keyDict)
+   #tested, now more than hopeful
    driver.session().run( \
    "MERGE (a:Article { link : $url } )\n" + \
    "SET a.keywordTime = timestamp()\n" + \
-   "FOREACH ( key in $keyDict | MERGE (a)--[:Has]->(b:Keyword { name : key } ) SET b.value = $keyDict.key )" + \
-   "FOREACH (label in $labels | SET a.label = $labels.label )", \
-   keyDict=keyDict, labels=labels, url=url)
+   "FOREACH ( key in keys($keyDict) | MERGE (a)-[:Has {certainty: $keyDict[key]} ]->(:Keyword {name : key})) "   \
+   , keyDict=keyDict, url=url)
 
 
 def dbSearch(searchString):
@@ -50,12 +37,12 @@ def dbSearch(searchString):
    "WHERE b.name in $keywords" + \
    "WITH b" + \
    "MATCH (a:Article)-[h:Has]->(b)" + \
-   "WITH a, sum(h.value) AS rank" + \
+   "WITH a, sum(h.certainty) AS rank" + \
    "return a ORDER BY rank DESC", \
    keywords=keywords)
 
 if __name__ == '__main__':
-   dbAdd('https://techcrunch.com/2017/10/06/apple-is-looking-into-reports-of-iphone-8-batteries-swelling/')
+   batchKeys()
    
    
      
